0a37746ff7c3700560d31febcb9c818878d3a299
DEVR01001 <rafael.dev01001@gmail.com>
2025-01-21 10:33:08 -0400

    Vet hone

---
+++ Public/js/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+      },
+      
+    pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    },
+});
